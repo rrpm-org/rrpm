c67d2eb4730f3dd1e80f00c3adfe80ebc1e5ff82
pybash1 <67195650+ax-a-dev@users.noreply.github.com>
2022-09-11 15:19:11 +0530

    feat(api): migrated all js presets to new api
    now all the js and ts presets use the new package manager and preset APIs

--- rrpm/rrpm.py
+++ rrpm/rrpm.py
@@ -16,13 +16,8 @@ from .presets.py import default_questions as py_q
 from .presets.py.poetry import poetry
 from .presets.py.pip import pip
 from .presets.py.venv import venv
-from .presets.js import default_questions as js_q
-from .presets.js.react import npm as rjnpm, yarn as rjyarn, pnpm as rjpnpm
-from .presets.ts.react import npm as rtnpm, yarn as rtyarn, pnpm as rtpnpm
-from .presets.js.nextjs import npm as njjnpm, yarn as njjyarn, pnpm as njjpnpm
-from .presets.ts.nextjs import npm as njtnpm, yarn as njtyarn, pnpm as njtpnpm
-from .presets.js.node import npm as njnpm, yarn as njyarn, pnpm as njpnpm
-from .presets.ts.node import npm as ntnpm, yarn as ntyarn, pnpm as ntpnpm
+from .presets.js import React, NextJS, Vanilla
+from .presets.js import React as ReactTS, NextJS as NextTS, Vanilla as VanillaTS
 from .utils import (
     get_home_dir,
     get_domain,
@@ -354,66 +349,45 @@ def create(name: str, src: bool = False):
         else:
             console.print("[red]Invalid package manager selected[/]")
     elif prj_type in ["NodeJS", "React", "NextJS"]:
-        lang, pkg = js_q()
-        if lang == -1:
-            for ext in config.config["extensions"]["hooks"]:
-                try:
-                    load_extension(
-                        os.path.expandvars(
-                            os.path.expanduser(config.config["root"]["ext_dir"])
-                        ),
-                        ext,
-                    ).pkg_fail(pkg)
-                except AttributeError:
-                    pass
-                except Exception:
-                    console.print(f"[red]Exception occurred in extension: {ext}[/]")
-                    console.print_exception()
-                    return
-        if lang == "TypeScript":
-            if prj_type == "React":
-                if pkg == "NPM":
-                    rtnpm(repository, name)
-                elif pkg == "Yarn":
-                    rtyarn(repository, name)
-                else:
-                    rtpnpm(repository, name)
-            elif prj_type == "NextJS":
-                if pkg == "NPM":
-                    njtnpm(repository, name)
-                elif pkg == "Yarn":
-                    njtyarn(repository, name)
-                else:
-                    njtpnpm(repository, name)
+        ts = questionary.confirm("Use TypeScript").ask()
+        if ts:
+            if prj_type == "NodeJS":
+                preset = VanillaTS(repository, name)
+                pms = {man.name: man for man in preset.package_managers}
+                pm = questionary.select("Package Manager", choices=pms.keys()).ask()
+                if not pm or not repository or not name: return
+                preset.generate(pms.get(pm))
+            elif prj_type == "React":
+                preset = ReactTS(repository, name)
+                pms = {man.name: man for man in preset.package_managers}
+                pm = questionary.select("Package Manager", choices=pms.keys()).ask()
+                if not pm or not repository or not name: return
+                preset.generate(pms.get(pm))
             elif prj_type == "NodeJS":
-                if pkg == "NPM":
-                    ntnpm(repository, name)
-                elif pkg == "Yarn":
-                    ntyarn(repository, name)
-                else:
-                    ntpnpm(repository, name)
+                preset = NextTS(repository, name)
+                pms = {man.name: man for man in preset.package_managers}
+                pm = questionary.select("Package Manager", choices=pms.keys()).ask()
+                if not pm or not repository or not name: return
+                preset.generate(pms.get(pm))
         else:
-            if prj_type == "React":
-                if pkg == "NPM":
-                    rjnpm(repository, name)
-                elif pkg == "Yarn":
-                    rjyarn(repository, name)
-                else:
-                    rjpnpm(repository, name)
-            elif prj_type == "NextJS":
-                if pkg == "NPM":
-                    njjnpm(repository, name)
-                elif pkg == "Yarn":
-                    njjyarn(repository, name)
-                else:
-                    njjpnpm(repository, name)
+            if prj_type == "NodeJS":
+                preset = Vanilla(repository, name)
+                pms = {man.name: man for man in preset.package_managers}
+                pm = questionary.select("Package Manager", choices=pms.keys()).ask()
+                if not pm or not repository or not name: return
+                preset.generate(pms.get(pm))
+            elif prj_type == "React":
+                preset = React(repository, name)
+                pms = {man.name: man for man in preset.package_managers}
+                pm = questionary.select("Package Manager", choices=pms.keys()).ask()
+                if not pm or not repository or not name: return
+                preset.generate(pms.get(pm))
             elif prj_type == "NodeJS":
-                if pkg == "NPM":
-                    njnpm(repository, name)
-                elif pkg == "Yarn":
-                    njyarn(repository, name)
-                else:
-                    njpnpm(repository, name)
+                preset = NextJS(repository, name)
+                pms = {man.name: man for man in preset.package_managers}
+                pm = questionary.select("Package Manager", choices=pms.keys()).ask()
+                if not pm or not repository or not name: return
+                preset.generate(pms.get(pm))
     else:
         for ext in exts:
             if ext.get(prj_type) is not None:
